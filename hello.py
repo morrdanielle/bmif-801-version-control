50712cbd9bc50da8de374ae7af427094f5424a02
danielle morrison <13dm94@queensu.ca>
2018-05-22 11:14:50 -0400

    ENH: added wednesday

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 print("hello world")
+<<<<<<< HEAD
 
 print("Today is Tuesday")
 print("Today is Tuesday")
 print("Tomorrow is Wednesday")
+=======
+>>>>>>> parent of 9d5c6e2... ENH: added square func and style
--- main.py
+++ main.py
@@ -1,6 +1,1 @@
 print("This is used for version controlling")
-
-def square_func(x):
-    return x**2
-
-print(square_func(10))
