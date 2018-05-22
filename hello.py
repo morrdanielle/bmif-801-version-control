8793d2237d28416bfa1fc8e7417e0d61e8d8157a
danielle morrison <13dm94@queensu.ca>
2018-05-22 11:15:43 -0400

    Revert "ENH: added wednesday"
    This reverts commit 50712cbd9bc50da8de374ae7af427094f5424a02.

--- hello.py
+++ hello.py
@@ -1,8 +1,5 @@
 print("hello world")
-<<<<<<< HEAD
 
 print("Today is Tuesday")
 print("Today is Tuesday")
 print("Tomorrow is Wednesday")
-=======
->>>>>>> parent of 9d5c6e2... ENH: added square func and style
